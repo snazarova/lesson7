98c03420d7199cbf560a24ffb22230b14bb151d1
ACER <snazarova0@>
2022-10-05 14:37:49 +0300

    дз законченное

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        // task1
+
+        for (int i = 0; i <= 10; i ++) {
+        System.out.println(i);
+        }
+
+        // task2
+
+        for (int i = 10; i >=1; i--) {
+         System.out.println(i);
+        }
+
+        // task3
+
+       for (int i = 0; i <= 17; i = i+2 ) {
+           System.out.println(i);
+       }
+
+       // task4
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
 }
