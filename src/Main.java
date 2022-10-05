3c67e200fa959e1e620585e420017d0d0d104126
ACER <snazarova0@>
2022-10-05 14:55:46 +0300

    дз законченное

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // task1
+        // Задание1 task1
 
         for (int i = 0; i <= 10; i ++) {
         System.out.println(i);
@@ -22,6 +22,24 @@ public class Main {
        // task4
 
         for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        // Задание2 task1
+
+        for (int i = 1904; i < 2096; i = i+4) {
+            System.out.println("Високосный год " + i);
+        }
+
+        // task2
+
+        for (int i = 7; i <= 98; i = i+7) {
+            System.out.println(i);
+        }
+
+        // task3
+
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
     }
